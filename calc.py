1cb6787bbcac38756747c27487aafd94ba8eb685
CANewbould <60323628+CANewbould@users.noreply.github.com>
2021-12-04 18:35:25 +0000

    Version 2
    Added a series of try - except clauses

--- calc.py
+++ calc.py
@@ -4,7 +4,9 @@
 
 # Author: C A Newbould
 
-# Version 1
+# Version 2
+
+## Added some try clauses
 
 print('\n\nThis app runs a simple calculator')
 print('----------------------------------\n')
@@ -21,16 +23,22 @@ print(" 'x' to exit\n")
 print("Then ,after the prompt, key in the two numbers to carry out the operation", end = ',')
 print(' with a space in-between\n')
 
+# Repeated values
+prompt = "Numbers" # Version 2
+invalid = "!!! INVALID OP CODE !!!" # Version 2
+codes = ['a', 's', 'm', 'd'] # Version 2
+
 def ask():
-    op = input("Operation: 'a', 's', 'd', 'm', or 'x': ")
-    if op == 'x':
-        return -999
-    elif op in ['a', 's', 'm', 'd']:
-        print("Numbers", end = ': ')
-        no1, no2 = map(float, input().split()) # functional, I like
-
+    op = input("Operation: " + ', '.join(codes) + " or 'x': ") # from simplilearn
+    if op in codes:
+        print(prompt, end = ': ')
+        while True: # Version 2
+            try: # Version 2
+                no1, no2 = map(float, input().split()) # functional, I like
+                break # Version 2
+            except ValueError: # Version 2
+                print('!!!Non-numeric!!!',prompt, end=': ') # Version 2
         # pity there is no 'switch' (could use map?)
-
         if op == 'a':
             return no1 + no2
         elif op =='s':
@@ -38,15 +46,26 @@ def ask():
         elif op == 'm':
             return no1 * no2
         elif op == 'd':
-            return no1 / no2
+            while True: # Version 2
+                try: # Version 2
+                    n = no1 / no2 # Version 2
+                    break # Version 2
+                except ZeroDivisionError: # Version 2
+                    print("!!!Zero divisor!!!", end = ' ') # Version 2
+                    no2 = float(input('New divisor: ')) # Version 2
+                except ValueError: # Version 2
+                    print(invalid) # Version 2
+            return n
+    elif op == 'x':
+        return op # Version 2
     else:
         print("!!! INVALID OP CODE !!!")
-        return 8888
+        return 888
 
 # Execution
 result = ask()
-while result != -999:
-    if result != 8888:
+while result != 'x': # Version 2
+    if result != 888:
         print("The result is: %g" % result)
     result = ask()
 
