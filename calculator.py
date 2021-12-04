1048c0c7773e7ff66493527c97a60cfa4114e228
CANewbould <60323628+CANewbould@users.noreply.github.com>
2021-12-04 17:32:07 +0000

    Now with 'try's added

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#calculator.py
+#calculator2.py
 
 # Python Program to Make a Simple Calculator
 
@@ -19,18 +19,35 @@ def divide(num1, num2):
 
 while True:
    print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction, Other-exit")
-   operation = int(input("Choose operation 1/2/3/4: "))
-
+   while True:
+      try:
+         operation = int(input("Choose operation 1/2/3/4: "))
+         break
+      except ValueError:
+         print("Only numbers are allowed!")
    if operation not in [1,2,3,4]:
       break
    else:
-      value1 = int(input("Enter 1st number: "))
-      value2 = int(input("Enter 2nd number: "))
-    
+      while True:
+         try:
+            value1 = int(input("Enter 1st number: "))
+            break
+         except ValueError:
+            print("MUST be an INTEGER")
+      while True:
+         try:
+            value2 = int(input("Enter 2nd number: "))
+            break
+         except ValueError:
+            print("MUST be an INTEGER")
    if operation == 1:
-      if value2 == 0:
-         value2 = int(input("Please enter a non-zero value: "))
-      print(value1, "/", value2, "=", divide(value1, value2))
+      while True:
+         try:
+            ret = divide(value1, value2)
+            break
+         except ZeroDivisionError:
+            value2 = int(input("Please enter a non-zero value: "))         
+      print(value1, "/", value2, "=", ret)
    elif operation == 2:
       print(value1, "*", value2, "=", multiplication(value1, value2))
    elif operation == 3:
